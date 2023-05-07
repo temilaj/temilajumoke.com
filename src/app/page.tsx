14ed0193d783435e7bc15bc3a710f53c205279da
temilaj <temi@temilajumoke.com>
2023-05-06 21:09:24 -0700

    update footer text for larger devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,16 +36,14 @@ export default function Home() {
         </h2>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Copyright © 2014 - {new Date().getFullYear()} Temi Lajumoke.
+          <p className={`m-0 text-sm opacity-50`}>
+            © 2014 - {new Date().getFullYear()} | Temi Lajumoke.
           </p>
         </a>
       </div>
