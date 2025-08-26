2dd15170256c09009d89ba58fd2466780e1afab3
Temi Lajumoke <temi@temilajumoke.com>
2025-08-25 17:59:30 -0700

    placate linter

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import React from 'react';
 import { useTheme } from '../hooks/useTheme';
 
 export default function ThemeToggle() {
--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Theme, ThemeContext } from './ThemeContext';
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
